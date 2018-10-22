8f6395fe60218bfbc4cbf787fde0a48270a7da10
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-10-22 15:11:22 +0300

    Added textarea to edit & edit bio

--- src/containers/profile/EditBio/index.js
+++ src/containers/profile/EditBio/index.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
-import { Container, Header, Left, Button, Icon, Text, Body, Title, Right, View } from 'native-base';
+import { Container, Header, Left, Button, Icon, Text, Body, Title, Right, View, Content, Textarea } from 'native-base';
+
+import s from './styles';
 
 class EditBio extends Component {
   render() {
@@ -15,11 +17,21 @@ class EditBio extends Component {
           <Body>
             <Title>Edit bio</Title>
           </Body>
-          <Right/>
+          <Right>
+            <Button transparent>
+              <Text>Save</Text>
+            </Button>
+          </Right>
         </Header>
-        <View>
-          <Text>hey</Text>
-        </View>
+        <Content>
+          <View style={s.wrapper}>
+            <Textarea
+              rowSpan={10}
+              bordered
+              style={s.textarea}
+              placeholder="Few words about you..."/>
+          </View>
+        </Content>
       </Container>
     );
   }
