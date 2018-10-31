69ff934c0a4f99bb3854d54eeb860ef03d3c21e2
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-10-31 19:27:21 +0300

    Closes #1 Removed role picker from sign in process

--- src/redux/ducks/auth/auth.js
+++ src/redux/ducks/auth/auth.js
@@ -1,35 +1,19 @@
 import { createAsyncAction, createReducer, createAction } from '../../../utils/actions';
 import { Map } from 'immutable';
 
-export const FETCH_FB_TOKEN = 'auth/auth/FETCH_FB_TOKEN';
 export const SIGN_IN = 'auth/auth/SING_IN';
-export const SIGN_UP = 'auth/auth/SIGN_UP';
 export const SIGN_OUT = 'auth/auth/SIGN_OUT';
 
-export const fetchFbToken = createAsyncAction(FETCH_FB_TOKEN);
 export const signIn = createAsyncAction(SIGN_IN);
-export const signUp = createAsyncAction(SIGN_UP);
 export const signOut = createAction(SIGN_OUT);
 
 const initialState = Map({
   authorized: false,
   jwt: '',
-  loading: false,
-  fbToken: ''
+  loading: false
 });
 
 export default createReducer({
-  [fetchFbToken.REQUEST]: (state) => state.merge({ loading: true }),
-
-  [fetchFbToken.SUCCESS]: (state, { payload }) => (
-    state.merge({
-      loading: false,
-      fbToken: payload
-    })
-  ),
-
-  [fetchFbToken.FAILURE]: (state) => state.merge({ loading: false }),
-
   [signIn.REQUEST]: (state) => state.merge({
     loading: true,
     jwt: '',
@@ -48,15 +32,5 @@ export default createReducer({
     authorized: false
   }),
 
-  [signUp.REQUEST]: (state) => state.merge({ loading: true }),
-  [signUp.SUCCESS]: (state) => state.merge({ loading: false }),
-  [signUp.FAILURE]: (state) => state.merge({ loading: false }),
-
-  [SIGN_OUT]: (state) => (
-    state.merge({
-      loading: false,
-      jwt: '',
-      authorized: false
-    })
-  )
+  [SIGN_OUT]: (state) => state.merge(initialState)
 }, initialState);
