8f3065b65b94f19147c66b67609e7d6c04e3577c
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-11-01 23:07:22 +0300

    This closes #4. Added android hardware back button behavior

--- src/containers/common/Main/index.js
+++ src/containers/common/Main/index.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
+import { BackHandler } from 'react-native';
 import { connect } from 'react-redux';
 import TabNavigator from '../../../navigation/TabNavigator';
 
 import { initSocket } from '../../../redux/ducks/chat/rooms';
 
 class Main extends Component {
+  constructor (props) {
+    super(props);
+    this.onBackPress = this.onBackPress.bind(this);
+  }
+
   componentWillMount() {
     this.props.initSocket();
+
+    BackHandler.addEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  componentWillUnmount () {
+    BackHandler.removeEventListener('hardwareBackPress', this.onBackPress);
+  }
+
+  onBackPress () {
+    const { dispatch, nav } = this.props;
+    dispatch(NavigationActions.back());
+    return nav !== this.props.nav;
   }
 
   render() {
@@ -15,7 +33,9 @@ class Main extends Component {
 }
 
 export default connect(
-  null,
+  (state) => ({ 
+    nav: state.nav 
+  }),
   {
     initSocket
   }
