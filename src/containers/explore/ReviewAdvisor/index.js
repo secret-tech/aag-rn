dde222600616508634a7033936824dd2710f50bd
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-11-12 17:36:49 +0300

    This closes #29. Fixed bug with overflow on feedback screen

--- src/containers/explore/ReviewAdvisor/index.js
+++ src/containers/explore/ReviewAdvisor/index.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { withNavigation, NavigationActions } from 'react-navigation';
 import { Image } from 'react-native';
-import { View, Text, Button } from 'native-base';
+import { Container, Content, View, Text, Button } from 'native-base';
 import StarRating from 'react-native-star-rating';
 import Icon from 'react-native-vector-icons/SimpleLineIcons';
 
@@ -60,56 +60,60 @@ class ReviewAdvisor extends Component {
     const { name, picture } = this.props.advisor.toJS();
 
     return (
-      <View style={s.container}>
-        <Button style={s.close} transparent onPress={() => this.nav('Explore', 'ExploreExplore')}>
-          <Icon name='arrow-left' size={24} color='#fff'/>
-        </Button>
-        
-        <View style={s.picWrap}>
-          <Image style={s.pic} source={{ uri: picture }} resizeMode="cover" />
-        </View>
+      <Container style={s.container}>
+        <Content>
+          <View style={s.wrap}>
+            <Button style={s.close} transparent onPress={() => this.nav('Explore', 'ExploreExplore')}>
+              <Icon name='arrow-left' size={24} color='#fff'/>
+            </Button>
+            
+            <View style={s.picWrap}>
+              <Image style={s.pic} source={{ uri: picture }} resizeMode="cover" />
+            </View>
 
-        <View style={s.textWrap}>
-          <Text style={s.ttl}>Send feedback for {name}!</Text>
+            <View style={s.textWrap}>
+              <Text style={s.ttl}>Send feedback for {name}!</Text>
 
-          <Text style={s.descr}>You recently spoke with {name}, please leave your feedback about the service provided.</Text>
-        </View>
+              <Text style={s.descr}>You recently spoke with {name}, please leave your feedback about the service provided.</Text>
+            </View>
 
-        <View style={s.stars}>
-          <View style={s.starsLabel}>
-            <Text style={s.starsLabelText}>{this.getLabel()}</Text>
+            <View style={s.stars}>
+              <View style={s.starsLabel}>
+                <Text style={s.starsLabelText}>{this.getLabel()}</Text>
+              </View>
+
+              <StarRating
+                disabled={false}
+                maxStars={5}
+                rating={this.state.rating}
+                selectedStar={(rating) => this.setState({ rating })}
+                emptyStar="star"
+                fullStar="star"
+                halfStar="star-half-o"
+                iconSet="FontAwesome"
+                fullStarColor="#e3b23c"
+                emptyStarColor="#ffe8d1"
+                starSize={50}
+                buttonStyle={{
+                  marginLeft: 5,
+                  marginRight: 5
+                }}
+                containerStyle={{
+                  marginLeft: -5,
+                  marginRight: -5
+                }}
+              />
+            </View>
+
+            <View style={s.buttonWrap}>
+              <Button transparent block style={s.button} onPress={this.send}>
+                <Text style={s.buttonText}>RATE</Text>
+              </Button>
+            </View>
+
           </View>
-
-          <StarRating
-            disabled={false}
-            maxStars={5}
-            rating={this.state.rating}
-            selectedStar={(rating) => this.setState({ rating })}
-            emptyStar="star"
-            fullStar="star"
-            halfStar="star-half-o"
-            iconSet="FontAwesome"
-            fullStarColor="#e3b23c"
-            emptyStarColor="#ffe8d1"
-            starSize={50}
-            buttonStyle={{
-              marginLeft: 5,
-              marginRight: 5
-            }}
-            containerStyle={{
-              marginLeft: -5,
-              marginRight: -5
-            }}
-          />
-        </View>
-
-        <View style={s.buttonWrap}>
-          <Button transparent block style={s.button} onPress={this.send}>
-            <Text style={s.buttonText}>RATE</Text>
-          </Button>
-        </View>
-
-      </View>
+        </Content>
+      </Container>
     );
   }
 }
