01c6159e0acf6e94322bea041c512cc0384510b6
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-12-05 04:43:18 +0300

    Basic implement peer-to-peer video calls

--- src/containers/chat/AudioCall/index.js
+++ src/containers/chat/AudioCall/index.js
@@ -1,25 +1,116 @@
 import React, { Component } from 'react';
-import { View } from 'native-base';
+import { View, Button, Text } from 'native-base';
 import io from 'socket.io-client';
 import { RTCPeerConnection, RTCMediaStream, RTCIceCandidate, RTCSessionDescription, RTCView, MediaStreamTrack, getUserMedia } from 'react-native-webrtc';
+
+const PEERS = {};
 
 export default class Call extends Component {
   constructor(props) {
     super(props);
 
     this.socket = io.connect('https://react-native-webrtc.herokuapp.com', { transports: ['websocket'] });
+    this.configuration = {'iceServers': [{ 'url': 'stun:stun.l.google.com:19302' }]};
 
     this.state = {
-      externalStreamURL: '',
-      internalStreamURL: ''
+      peers: {},
+      externalStream: '',
+      internalStream: ''
     };
   }
 
+  componentDidMount() {
+    this.getInternalStream(true, (stream) => this.setState({ internalStream: stream }));
+
+    this.socket.on('exchange', (data) => this.exchange(data));
+    this.socket.on('connect', (data) => {
+      this.getInternalStream(true, (stream) => this.setState({ internalStream: stream }));
+    });
+  }
+
+  join = (roomId) => {
+    this.socket.emit('join', roomId, (socketIds) => {
+      for (const index in socketIds) {
+        const socketId = socketIds[index];
+        this.createPeerConnection(socketId, true);
+      }
+    });
+  }
+
+  exchange = (data) => {
+    // const peerConn = this.createPeerConnection(data.from, false);
+    let peerConnection;
+    if (data.from in PEERS) {
+      peerConnection = PEERS[data.from];
+    } else {
+      peerConnection = this.createPeerConnection(data.from, false);
+    }
+
+    if (data.sdp) {
+      peerConnection.setRemoteDescription(new RTCSessionDescription(data.sdp), () => {
+        if (peerConnection.remoteDescription.type === 'offer') {
+          peerConnection.createAnswer((description) => {
+            console.log('create answer', description);
+            peerConnection.setLocalDescription((description) => {
+              console.log('set local description', peerConnection.localDescription);
+              this.socket.emit('exchange', {'to': fromId, 'sdp': peerConnection.localDescription });
+            }, (e) => console.log(e));
+          }, (e) => console.log(e));
+        }
+      }, (e) => console.log(e));
+    } else {
+      console.log('exchange candidate', data);
+      peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
+    }
+  }
+
+  createPeerConnection = (peerId, isOffer) => {
+    const peerConnection = new RTCPeerConnection(this.configuration);
+
+    PEERS[peerId] = peerConnection;
+
+    const createOffer = () => {
+      peerConnection.createOffer((description) => {
+        console.log('create offer', description);
+        peerConnection.setLocalDescription(description, () => {
+          console.log('set local description', description);
+          this.socket.emit('exchange', { to: peerId, sdp: peerConnection.localDescription });
+        }, (e) => console.log(e));
+      }, (e) => console.log(e));
+    }
+
+    peerConnection.onaddstream = (event) => this.setState({ externalStream: event.stream });
+    peerConnection.onnegotiationneeded = () => isOffer ? createOffer() : null;
+    peerConnection.addStream(this.state.internalStream);
+  }
+
+  getInternalStream = (isFrontSource, callback) => {
+    const options = {
+      audio: true,
+      video: {
+        facingMode: 'user',
+        mandatory: {
+          minWidth: 640,
+          minHeight: 360,
+          minFrameRate: 30
+        }
+      }
+    };
+
+    getUserMedia(options, (stream) => callback(stream), (e) => console.log(e));
+  }
+
   render() {
+    const {
+      externalStream,
+      internalStream
+    } = this.state;
+
     return (
       <View style={s.main}>
-        <RTCView style={s.externalVideo} streamURL={this.state.externalStreamURL}/>
-        <RTCView style={s.internalVideo} streamURL={this.state.internalStreamURL}/>
+        <RTCView style={s.externalVideo} streamURL={externalStream && externalStream.toURL()}/>
+        <Button onPress={() => this.join('pidor')}><Text>JOIN</Text></Button>
+        <RTCView style={s.internalVideo} streamURL={internalStream && internalStream.toURL()}/>
       </View>
     );
   }
