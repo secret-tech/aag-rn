31721bdf7b834a418614e8df9b7a924eb6e40890
Aidar Ibatullin <amazing.space.invader@gmail.com>
2018-12-09 05:39:44 +0300

    Added comments about our webrtc implementation

--- src/containers/chat/Call/index.js
+++ src/containers/chat/Call/index.js
@@ -1,3 +1,11 @@
+// All comments in russian language coz I'm too lazy to rewrite them. Enjoy :-)
+
+// Оставь надежду, всяк сюда входящий.
+// Подумай 10 раз, перед тем, как что-то менять в этом проклятом файле. Тут тебе не рады.
+// Тут используется deprecated webrtc DOM API потому что пакет react-native-webrtc еще не поддерживает актуальную версию.
+// Для понимания какого хуя здесь происходит - советую прочитать документацию WEBRTC на mdn полностью
+// https://developer.mozilla.org/ru/docs/Web/API/WebRTC_API
+
 import React, { Component } from 'react';
 import { Dimensions, Image } from 'react-native';
 import { View, Button, Text } from 'native-base';
@@ -10,6 +18,7 @@ import socketService from '../../../utils/socketService';
 
 import s from './styles';
 
+// Конфигурация локальных медиа данных
 const LOCAL_STREAM_OPTIONS = {
   audio: true,
   video: {
@@ -22,6 +31,7 @@ const LOCAL_STREAM_OPTIONS = {
   }
 };
 
+// Устаревшие опции для создания offer. По-умолчанию они являются true, а тут находятся на всякий случай
 const OFFER_OPTIONS = {
   offerToReceiveAudio: true,
   offerToReceiveVideo: true
@@ -44,33 +54,31 @@ class Call extends Component {
   }
 
   async componentWillMount() {
-    // Создаем пир конекшоны
+    // 1 Создаем RTC Peer Connection
     this.localPeer = new RTCPeerConnection(this.configuration);
     this.remotePeer = new RTCPeerConnection(this.configuration);
 
-    // обработчик айс кандидатов
+    // 2 Handle ICE candidates and sync that shit between peers via socket
     this.localPeer.onicecandidate = ({ candidate }) => {
-      console.log('onicecandidate', candidate);
       const body = { conversationId: this.props.navigation.state.params.conversationId, candidate };
       if (candidate) this.socket.emit('req:ice', body);
     };
 
-    // Создаем локал стрим
+    // Create local stream (video, audio)
     await getUserMedia(
       LOCAL_STREAM_OPTIONS, 
       (stream) => this.setState({ localStream: stream }), 
       (e) => console.log(e)
     );
 
-    // добавляем треки в локал пир
+    // Add localStream to localPeer
     this.localPeer.addStream(this.state.localStream);
 
-    // инитим сокет
+    // Init socket client and they handlers
     await this.initSocket();
 
-    // хандлим трек
+    // Handle adding stream and put them into the state
     this.localPeer.onaddstream = (event) => {
-      console.log('onaddstream', event);
       this.setState({ remoteStream: event.stream });
     };
   }
@@ -78,69 +86,67 @@ class Call extends Component {
   componentWillUnmount() {}
 
   async initSocket() {
+    // Create socket instance
     this.socket = await socketService();
 
-    // Делать то, что скажет сервер
+    // Do what the server say
+    // Сервер определяет кто является caller и присылает этот ивент клиенту caller
     this.socket.on('res:uCaller', this.createOffer);
+
+    // После того, как caller выполнил req:offer, callee получит событие res:offer
     this.socket.on('res:offer', this.handleIncomingOffer);
+
+    // После того, как callee принял description присылаем событие caller-у 
     this.socket.on('res:answer', this.handleAnswer);
+
+    // Синхронизируем ICE сервера между клиентами
     this.socket.on('res:ice', this.handleNewIceCandidate);
 
-    // Сказать серверы что клиент готов к труду о обороне в conversationId
+    // Сказать серверу, что клиент готов принимать всякое
     this.socket.emit('req:imReady', this.props.navigation.state.params.conversationId);
   }
 
-  // запускаем при res:uCaller
-  // p1 создаем офер и устанаваливаем локал дескр для себя
-  // эмитим событие req:offer
+  // Запускается при сокет событии res:uCaller
+  // [CALLER] создает offer и устанавливает description
+  // При успехе всей цепи порождает событие req:offer
   createOffer = () => {
-    console.log('createOffer called');
     this.localPeer.createOffer((description) => {
-      console.log('localPeer.createOffer', description);
       this.localPeer.setLocalDescription(description, () => {
         const body = { conversationId: this.props.navigation.state.params.conversationId, description };
-        console.log('res:uCaller', body);
         this.socket.emit('req:offer', body);
       }, (e) => console.log(e));
     }, (e) => console.log(e), OFFER_OPTIONS);
   }
 
-  // запускаем при res:offer
-  // p2 устанавливаем ремоут дескр для себя и вызываем ответ
+  // Запускается при сокет событии res:offer
+  // [CALLEE] устанавливает description и вызывает следующую функцию
   handleIncomingOffer = ({ description }) => {
-    console.log('handleIncomingOffer', description);
     this.localPeer.setRemoteDescription(new RTCSessionDescription(description), () => {
-      console.log('handleIncomingOffer setRemoteDescription', description);
       this.createAnswer();
     }, (e) => console.log(e));
   }
 
-  // вызывается после handleIncomingOffer
-  // p2 создаем ответ и устанавливаем ремоут дескр для себя
-  // эмитим событие req:asnwer
+  // [CALLEE] создает answer description, устанавливает его для себя и передает другому пиру
+  // При успехе порождает событие req:asnwer
   createAnswer = () => {
-    console.log('createAnswer');
     this.localPeer.createAnswer((description) => {
-      console.log('createAnswer', description);
       this.localPeer.setLocalDescription(description, () => {
-        console.log('createAnswer setLocalDescription', description);
         const body = { conversationId: this.props.navigation.state.params.conversationId, description };
         this.socket.emit('req:answer', body);
       }, (e) => console.log(e));
     }, (e) => console.log(e));
   }
 
-  // запускаем при res:answer
-  // p1 пришел ответ, устанавливаем ремоут дескр для себя
+  // Запускается при сокет событии res:answer
+  // [CALLER] получает ответ от callee и устанавливает description
   handleAnswer = ({ description }) => {
-    console.log('handleAnswer', description);
-    this.localPeer.setRemoteDescription(new RTCSessionDescription(description), () => {
-      console.log('handleAnswer setRemoteDescription', description);
-    }, (e) => console.log(e));
+    // не трогать аргументы!!! Почему-то без них не совсем работает, я хз
+    this.localPeer.setRemoteDescription(new RTCSessionDescription(description), () => {}, (e) => console.log(e));
   }
 
+  // Запускается при сокет событии res:ice
+  // Любой клиент получает нового ICE кандидата и устанавливает его в RTCPC
   handleNewIceCandidate = ({ candidate }) => {
-    console.log('handleNewIceCandidate', new RTCIceCandidate(candidate));
     this.localPeer.addIceCandidate(new RTCIceCandidate(candidate));
   }
 
@@ -149,8 +155,6 @@ class Call extends Component {
       localStream,
       remoteStream
     } = this.state;
-
-    console.log('streams', localStream, remoteStream);
 
     return (
       <View style={s.container}>
@@ -174,166 +178,3 @@ class Call extends Component {
 
 
 export default Call;
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const PEERS = {};
-
-// export default class Call extends Component {
-//   constructor(props) {
-//     super(props);
-
-//     this.socket = null;
-//     this.configuration = {'iceServers': [{ 'url': 'stun:stun.l.google.com:19302' }]};
-
-//     this.state = {
-//       peers: {},
-//       externalStream: {},
-//       internalStream: {}
-//     };
-//   }
-
-//   async initSocket() {
-//     this.socket = await socketService();
-//   }
-
-//   async componentDidMount() {
-//     await this.initSocket();
-
-//     this.getInternalStream(true, (stream) => this.setState({ internalStream: stream }));
-
-//     this.socket.on('exchange', (data) => this.exchange(data));
-//     // this.socket.on('connect', () => {
-//     //   this.getInternalStream(true, (stream) => this.setState({ internalStream: stream }));
-//     // });
-
-//     console.log('join to call with ', this.props.navigation.state.params.conversationId);
-
-//     this.join(this.props.navigation.state.params.conversationId);
-//   }
-
-//   componentWillUnmount() {
-//     this.socket.off('exchange');
-//     this.socket.off('connect');
-//   }
-
-//   join = (conversationId) => {
-//     console.log('emit join', conversationId);
-//     this.socket.emit('join', conversationId, (socketIds) => {
-//       for (const index in socketIds) {
-//         const socketId = socketIds[index];
-//         this.createPeerConnection(socketId, true);
-//       }
-//     });
-//   }
-
-//   exchange = (data) => {
-//     console.log('exchange happend', data);
-//     // const peerConn = this.createPeerConnection(data.from, false);
-//     let peerConnection;
-//     if (data.from in PEERS) {
-//       peerConnection = PEERS[data.from];
-//     } else {
-//       peerConnection = this.createPeerConnection(data.from, false);
-//     }
-
-//     if (data.sdp) {
-//       peerConnection.setRemoteDescription(new RTCSessionDescription(data.sdp), () => {
-//         if (peerConnection.remoteDescription.type === 'offer') {
-//           peerConnection.createAnswer((description) => {
-//             console.log('create answer', description);
-//             peerConnection.setLocalDescription(description, () => {
-//               console.log('set local description', peerConnection.localDescription);
-//               this.socket.emit('exchange', { to: data.from, sdp: peerConnection.localDescription });
-//             }, (e) => console.log(e));
-//           }, (e) => console.log(e));
-//         }
-//       }, (e) => console.log(e));
-//     } else {
-//       console.log('exchange candidate', data);
-//       peerConnection.addIceCandidate(new RTCIceCandidate(data.candidate));
-//     }
-//   }
-
-//   createPeerConnection = (peerId, isOffer) => {
-//     const peerConnection = new RTCPeerConnection(this.configuration);
-
-//     PEERS[peerId] = peerConnection;
-
-//     const createOffer = () => {
-//       peerConnection.createOffer((description) => {
-//         console.log('create offer', description);
-//         peerConnection.setLocalDescription(description, () => {
-//           console.log('set local description', description);
-//           this.socket.emit('exchange', { to: peerId, sdp: peerConnection.localDescription });
-//         }, (e) => console.log(e));
-//       }, (e) => console.log(e));
-//     }
-
-//     peerConnection.onaddstream = (event) => {
-//       console.log(event);
-//       this.setState({ externalStream: event.stream })
-//     };
-//     peerConnection.onnegotiationneeded = () => isOffer ? createOffer() : null;
-//     peerConnection.addStream(this.state.internalStream);
-
-//     return peerConnection;
-//   }
-
-//   getInternalStream = (isFrontSource, callback) => {
-//     const options = {
-//       audio: true,
-//       video: {
-//         facingMode: 'user',
-//         mandatory: {
-//           minWidth: Dimensions.get('window').width,
-//           minHeight: Dimensions.get('window').height,
-//           minFrameRate: 30
-//         }
-//       }
-//     };
-
-//     getUserMedia(options, (stream) => callback(stream), (e) => console.log(e));
-//   }
-
-//   render() {
-//     const {
-//       externalStream,
-//       internalStream
-//     } = this.state;
-
-//     console.log('streams', externalStream, internalStream);
-
-//     return (
-//       <View style={s.container}>
-//         <View style={s.externalVideoContainer}>
-//           <RTCView objectFit="cover" style={s.externalVideo} streamURL={externalStream.toURL && externalStream.toURL()}/>
-//         </View>
-        
-//         <View style={s.internalVideoContainer}>
-//           <RTCView objectFit="cover" style={s.internalVideo} streamURL={internalStream.toURL && internalStream.toURL()}/>
-//         </View>
-
-//         <View style={s.controls}>
-//           <View style={s.button}>
-//             <CallButton iconName="call-end" backgroundColor="#ff3b2f" iconColor="#fff"/>
-//           </View>
-//         </View>
-//       </View>
-//     );
-//   }
-// }
